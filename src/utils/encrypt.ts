a8ec986f77de567f8fd0070b147298fcc8cd2005
TangYanfei <don1343@foxmail.com>
2020-06-06 16:24:54 +0800

    update:检查更新接口修改

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,8 +1,8 @@
 import {NativeModules} from 'react-native';
-import JSEncrypt from 'jsencrypt';
+// import JSEncrypt from 'jsencrypt';
 
-const pubkey =
-  'MMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC47SZrn8veaJMTfLYFGOyHS1X5wSXlPkxFssmKciudtewz/uMAlNubW1KUKNJuacUK1D6huZB2IKDM17rEVx00VzTiV9ZlskMNKgDbAX8clz66npwp3a2egmzDCi7N+NYqV27xRifvg3cJ+AsTQlFibt/jRg7eHfE1yM7VTJlhBQIDAQAB';
+// const pubkey =
+//   'MMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC47SZrn8veaJMTfLYFGOyHS1X5wSXlPkxFssmKciudtewz/uMAlNubW1KUKNJuacUK1D6huZB2IKDM17rEVx00VzTiV9ZlskMNKgDbAX8clz66npwp3a2egmzDCi7N+NYqV27xRifvg3cJ+AsTQlFibt/jRg7eHfE1yM7VTJlhBQIDAQAB';
 
 const Aes = NativeModules.Aes;
 
@@ -22,82 +22,38 @@ const encryptData = async (text: string, key: any) => {
   });
 };
 
-const randomWord = (randomFlag: boolean, min: number, max: number) => {
-  let str = '';
-  let range = min;
-  let arr = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
+// const randomWord = (randomFlag: boolean, min: number, max: number) => {
+//   let str = '';
+//   let range = min;
+//   let arr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//   // 随机产生
+//   if (randomFlag) {
+//     range = Math.round(Math.random() * (max - min)) + min;
+//   }
+//   for (let i = 0; i < range; i++) {
+//     var pos = Math.round(Math.random() * (arr.length - 1));
+//     str += arr[pos];
+//   }
+//   return str;
+// };
 
-  // 随机产生
-  if (randomFlag) {
-    range = Math.round(Math.random() * (max - min)) + min;
-  }
-  for (let i = 0; i < range; i++) {
-    var pos = Math.round(Math.random() * (arr.length - 1));
-    str += arr[pos];
-  }
-  return str;
-};
+// export const rand = randomWord(true, 16, 16);
 
-export const rand = randomWord(true, 16, 16);
+// export const getCode = (pass: string) => {
+//   var encrypt = new JSEncrypt();
+//   encrypt.setPublicKey(pubkey);
+//   var d = encrypt.encrypt(pass);
 
-export const getCode = (pass: any) => {
-  var encrypt = new JSEncrypt();
+//   var ps = encodeURI(d).replace(/\+/g, '%2B');
+//   return ps;
+// };
 
-  encrypt.setPublicKey(pubkey);
-  var d = encrypt.encrypt(pass);
-  var ps = encodeURI(d).replace(/\+/g, '%2B');
-  return ps;
-};
-
-export const encrypt = async (params: any) => {
+export const encrypt = async (params: string) => {
   try {
     const genKey = await generateKey('Arnold', 'salt', 5000, 256);
 
     const enData = await encryptData(params, genKey);
 
-    // const rsaEncrpypt = Rsa.encrypt(enData.iv)
-
-    // return {
-    //   appKey: rsaEncrpypt,
-    //   params: enData.cipher
-    // }
     return enData;
   } catch (e) {
     console.error(e);
